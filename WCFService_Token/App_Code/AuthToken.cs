353efed8935b8eb05f28bc15ef5e2d854946409c
Mauro Crispim <mauro.crispim@cm-vfxira.pt>
2019-05-20 17:29:14 +0100

    Add project files.

---
+++ WCFService_Token/App_Code/AuthToken.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for AuthToken
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class AuthToken : System.Web.Services.WebService {
+
+    public SecuredTokenWebservice SoapHeader;
+
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public string AuthenticationUser()
+    {
+        if (SoapHeader == null)
+            return "Please provide Username and Password";
+        if (string.IsNullOrEmpty(SoapHeader.UserName) || string.IsNullOrEmpty(SoapHeader.Password))
+            return "Please provide Username and Password";
+
+        //Check is User credentials Valid
+        if (!SoapHeader.IsUserCredentialsValid(SoapHeader.UserName,SoapHeader.Password))
+            return "Invalid Username or Password";
+
+        // Create and store the AuthenticatedToken before returning it
+        string token = Guid.NewGuid().ToString();
+        HttpRuntime.Cache.Add(
+            token,
+            SoapHeader.UserName,
+            null,
+            System.Web.Caching.Cache.NoAbsoluteExpiration,
+            TimeSpan.FromMinutes(30),
+            System.Web.Caching.CacheItemPriority.NotRemovable,
+            null
+            );
+
+        return token;
+    }
+
+    [WebMethod]
+    [System.Web.Services.Protocols.SoapHeader("SoapHeader")]
+    public string HelloWorld() 
+    {
+        if (SoapHeader == null)
+            return "Please call AuthenticationMethod() first.";
+
+        if(!SoapHeader.IsUserCredentialsValid(SoapHeader))
+            return "Please call AuthenticationMethod() first.";
+
+        return "Hello " + HttpRuntime.Cache[SoapHeader.AuthenticationToken];
+    }
+    
+}
---
+++ WCFService_Token/App_Code/SecuredTokenWebservice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SecuredTokenWebservice
+/// </summary>
+public class SecuredTokenWebservice : System.Web.Services.Protocols.SoapHeader
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string AuthenticationToken { get; set; }
+
+    public bool IsUserCredentialsValid(string UserName, string Password)
+    {
+        //database connection to validate the User
+        if (UserName == "admin" && Password == "admin")
+            return true;
+        else
+            return false;
+    }
+
+    public bool IsUserCredentialsValid(SecuredTokenWebservice SoapHeader)
+    {
+        if (SoapHeader == null)
+            return false;
+
+        // check the token exists in Cache
+        if (!string.IsNullOrEmpty(SoapHeader.AuthenticationToken))
+            return (HttpRuntime.Cache[SoapHeader.AuthenticationToken] != null);
+
+        return false;
+    }
+}
